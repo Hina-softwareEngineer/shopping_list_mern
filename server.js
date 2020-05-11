1efa5a4245b2a04034cdbc8610b405a746a3223a
HINA KHADIM <hinakhadim2002@gmail.com>
2020-05-11 17:04:32 +0500

    adding config for heroku

--- server.js
+++ server.js
@@ -21,22 +21,22 @@ const db = config.get('mongoURI');
 
 // Connect to Mongo
 
-mongoose.connect(db, { useNewUrlParser : true, useUnifiedTopology: true, useCreateIndex : true})
-        .then(()=> console.log('MongoDB Connected...'))
-        .catch(err => console.log(err));
+mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
+    .then(() => console.log('MongoDB Connected...'))
+    .catch(err => console.log(err));
 
 // routes
-app.use('/api/items',  require('./routes/api/items'));
-app.use('/api/users',  require('./routes/api/users'));
-app.use('/api/auth',  require('./routes/api/auth'));
+app.use('/api/items', require('./routes/api/items'));
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
 
 // server static assets if in production
 
-if(process.env.NODE_ENV === 'production'){
+if (process.env.NODE_ENV === 'production') {
     // Set Static folder
     app.use(express.static('client/build'));
-    
-    app.get('*', (req,res) => {
+
+    app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
     });
 }
