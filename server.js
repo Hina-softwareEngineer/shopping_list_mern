4b81f2eaa22952f8ed4f515854378bdb345c8a99
HINA KHADIM <hinakhadim2002@gmail.com>
2020-05-29 17:15:08 +0500

    pwa

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 // const bodyParser = require("body-parser");
 const path = require('path');
 const config = require('config');
-
+// const enforce = require('express-sslify');
 
 const app = express();
 
@@ -13,6 +13,7 @@ const app = express();
 // app.use(bodyParser.urlencoded({ extended : true }))
 
 app.use(express.json())
+// app.use(enforce.HTTPS({ trustProtoHeader: true }));
 
 
 // DB config
@@ -45,4 +46,9 @@ const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
     console.log("Server Started on Port : ", port);
+});
+
+
+app.get('/service-worker.js', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '..', 'build', 'service-worker.js'));
 });
